102c5e009016a1d5f6cd05b97a18c095a7458b1c
a23axehe <a23axehe@student.his.se>
2024-05-08 14:00:04 +0200

    Lade till text i about.html

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -1,14 +1,32 @@
 package com.example.project;
 
+import android.os.Bundle;
+import android.webkit.WebViewClient;
+import android.webkit.WebView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AboutActivity extends AppCompatActivity {
+    private WebView aboutWebView;
+    private class aboutWebViewClient extends WebViewClient{
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+    }
+    public void showInternalWebPage() {
+        aboutWebView.loadUrl("file:///android_asset/about.html");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        aboutWebView = findViewById(R.id.my_webview);
+
+        aboutWebView.getSettings().setJavaScriptEnabled(true);
+
+        WebViewClient webViewClient = new aboutWebViewClient();
+        aboutWebView.setWebViewClient(webViewClient);
+        showInternalWebPage();
     }
 }
